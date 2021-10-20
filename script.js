782034b9f535ebc7e68df55dc729a1bec2c724cc
Zbin Official <48700650+Zobin33@users.noreply.github.com>
2021-10-20 19:21:50 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function nginput(value){
+     document.getElementById("numberinput").value = (document.getElementById("numberinput").value || null) + value
+     return 0
+     }
+function calculate(){
+       let str = document.getElementById("numberinput").value.toString()
+       str = str.toLowerCase().replace(/x/g,'*').replace(/\:/g,'/')
+       document.getElementById("numberinput").value = eval(str)
+     }
