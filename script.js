88b6dd014e962b93abe94a3b229417ee19d59e92
Zbin Official <48700650+Zobin33@users.noreply.github.com>
2021-10-21 09:21:39 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function nginput(value){
-     document.getElementById("numberinput").value = (document.getElementById("numberinput").value || null) + value
-     return 0
-     }
-function calculate(){
-       let str = document.getElementById("numberinput").value.toString()
-       str = str.toLowerCase().replace(/x/g,'*').replace(/\:/g,'/')
-       document.getElementById("numberinput").value = eval(str)
-     }
+function nginput(value) {
+  document.getElementById("numberinput").innerHTML = (document.getElementById("numberinput").innerHTML == 'Masukan Nomor' ? null: document.getElementById("numberinput").innerHTML) + value
+}
+
+function reset() {
+  document.getElementById('numberinput').innerHTML = 'Masukan Nomor'
+}
+
+function calculate() {
+  let str = document.getElementById("numberinput").innerHTML.toString()
+  str = str.toLowerCase().replace(/x/g, '*').replace(/\:/g, '/')
+  document.getElementById("numberinput").innerHTML = eval(str)
+}
