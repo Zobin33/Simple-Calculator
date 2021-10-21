c89964cd31e8f61efbfe753bce9f4d07667e95c8
Zbin Official <48700650+Zobin33@users.noreply.github.com>
2021-10-21 11:38:09 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 function input(value) {
-  let operator = [":",
-    "x",
+  let operator = ["÷",
+    "×",
     "+",
     "-",
     "%"]
   let input_value = document.getElementById("input-bang").innerHTML.replace("Masukan Nomor", '')
   let result;
-  if (operator.includes(input_value[input_value.length-1]) && operator.includes(value)) {
-    result = input_value.slice(0, input_value.length-1) + value
+  if (operator.includes(input_value[input_value.length-2]) && operator.includes(value.slice(1, 2))) {
+    result = input_value.slice(0, input_value.length-3) + value
+  } else {
+    result = input_value + value
   }
-  result = input_value + value
   document.getElementById("input-bang").innerHTML = result
-  result = result.replace(/x/g, '*').replace(/\:/g, '/')
+  result = result.replace(/×/g, '*').replace(/\:/g, '/').replace(/\,/g, '.')
   document.getElementById("before-result").innerHTML = eval(result)
 }
 
 function reset() {
   document.getElementById('input-bang').innerHTML = 'Masukan Nomor'
-  document.getElementById('before-result').innerHTML = ''
+  document.getElementById('before-result').innerHTML = '  '
 }
 
 function result() {
   let str = document.getElementById("input-bang").innerHTML
-  str = str.replace(/x/g, '*').replace(/\:/g, '/')
+  str = str.replace(/×/g, '*').replace(/\:/g, '/').replace(/\,/g, '.')
   document.getElementById("input-bang").innerHTML = eval(str)
+  document.getElementById('before-result').innerHTML = '  '
+}
+
+function delete_input() {
+  let value = document.getElementById('input-bang').innerHTML.toString()
+  let clear = value.slice(0, value.length-1)
+  document.getElementById('input-bang').innerHTML = clear
+  let akhir = clear.replace(/×/g, '*').replace(/\:/g, '/').replace(/\,/g, '.')
+  document.getElementById('before-result').innerHTML = eval(akhir) || ' '
 }
