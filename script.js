3c685677c4f24e22a2ed6ee4bc97e19ec791c953
Zbin Official <48700650+Zobin33@users.noreply.github.com>
2021-10-21 12:34:58 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let hasResult = false
 function input(value) {
   let operator = ["÷",
     "×",
@@ -12,27 +13,38 @@ function input(value) {
   } else {
     result = input_value + value
   }
+  if (hasResult) {
+    result = value
+    hasResult = false
+  }
   document.getElementById("input-bang").innerHTML = result
   result = result.replace(/×/g, '*').replace(/\:/g, '/').replace(/\,/g, '.')
-  document.getElementById("before-result").innerHTML = eval(result)
+  document.getElementById("before-result").innerHTML = '= ' + (eval(result) || '')
 }
 
 function reset() {
   document.getElementById('input-bang').innerHTML = 'Masukan Nomor'
-  document.getElementById('before-result').innerHTML = '  '
+  document.getElementById('before-result').innerHTML = ''
 }
 
 function result() {
   let str = document.getElementById("input-bang").innerHTML
   str = str.replace(/×/g, '*').replace(/\:/g, '/').replace(/\,/g, '.')
-  document.getElementById("input-bang").innerHTML = eval(str)
-  document.getElementById('before-result').innerHTML = '  '
+  document.getElementById("input-bang").innerHTML = eval(str) || 0
+  document.getElementById('before-result').innerHTML = ""
+  hasResult = true
 }
 
 function delete_input() {
   let value = document.getElementById('input-bang').innerHTML.toString()
-  let clear = value.slice(0, value.length-1)
-  document.getElementById('input-bang').innerHTML = clear
-  let akhir = clear.replace(/×/g, '*').replace(/\:/g, '/').replace(/\,/g, '.')
-  document.getElementById('before-result').innerHTML = eval(akhir) || ' '
+  console.log(value.length)
+  if (value.length !== 13 && value.length !== 1 && value.length !== 3) {
+    let clear = value.slice(0, value.length-1)
+    document.getElementById('input-bang').innerHTML = clear
+    let akhir = clear.replace(/×/g, '*').replace(/\:/g, '/').replace(/\,/g, '.')
+    document.getElementById('before-result').innerHTML = '= ' (eval(akhir) || '')
+  } else {
+    document.getElementById('input-bang').innerHTML = 'Masukan Nomor'
+    document.getElementById('before-result').innerHTML = ''
+  }
 }
