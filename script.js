f262b4a9efd577e463bf941359f06d1dbfb3b0e7
Zbin Official <48700650+Zobin33@users.noreply.github.com>
2021-10-21 09:55:20 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-function nginput(value) {
-  document.getElementById("numberinput").innerHTML = (document.getElementById("numberinput").innerHTML == 'Masukan Nomor' ? null: document.getElementById("numberinput").innerHTML) + value
+function input(value) {
+  let operator = [":",
+    "x",
+    "+",
+    "-",
+    "%"]
+  let input_value = document.getElementById("input-bang").innerHTML.replace("Masukan Nomor", '')
+  let result;
+  if (operator.includes(input_value[input_value.length-1]) && operator.includes(value)) {
+    result = input_value.slice(0, input_value.length-1) + value
+  }
+  result = input_value + value
+  document.getElementById("input-bang").innerHTML = result
+  result = result.replace(/x/g, '*').replace(/\:/g, '/')
+  document.getElementById("before-result").innerHTML = eval(result)
 }
 
 function reset() {
-  document.getElementById('numberinput').innerHTML = 'Masukan Nomor'
+  document.getElementById('input-bang').innerHTML = 'Masukan Nomor'
+  document.getElementById('before-result').innerHTML = ''
 }
 
-function calculate() {
-  let str = document.getElementById("numberinput").innerHTML.toString()
-  str = str.toLowerCase().replace(/x/g, '*').replace(/\:/g, '/')
-  document.getElementById("numberinput").innerHTML = eval(str)
+function result() {
+  let str = document.getElementById("input-bang").innerHTML
+  str = str.replace(/x/g, '*').replace(/\:/g, '/')
+  document.getElementById("input-bang").innerHTML = eval(str)
 }
